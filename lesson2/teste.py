203b3d364613d04c8530f5e8de8008b926cbab5f
riulado <joaomagalhaes810714@gmail.com>
2025-04-01 22:21:37 +0100

    r

--- lesson2/teste.py
+++ lesson2/teste.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 
-df_conjunto = pd.read_csv('shopping_trends.csv')
-print(df_conjunto.info())
-print(df_conjunto.head())
+df_conjunto = pd.read_csv('03 shopping_trends.csv')
+print(df_conjunto)
 
