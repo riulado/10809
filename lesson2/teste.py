5366d702e1b18844d472a1da9c26528ad1b4c10d
riulado <joaomagalhaes810714@gmail.com>
2025-04-03 21:04:05 +0100

    20250403

--- lesson2/teste.py
+++ lesson2/teste.py
@@ -3,5 +3,9 @@ import pandas as pd
 
 df_conjunto = pd.read_csv('03 shopping_trends.csv')
 print(df_conjunto.info())
-print(df_conjunto.head(20))
-
+print(df_conjunto.head())
+print(df_conjunto.describe())
+print(df_conjunto.tail())
+print(df_conjunto.columns)
+print(df_conjunto['Customer ID'].describe())
+print(df_conjunto['Customer ID'].unique())
